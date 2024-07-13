59a99b31cd89a4888e5c4456e6bf41cc0094d762
nicolocho03 <gomezcano2003@gmail.com>
2024-07-13 13:21:36 -0500

    Creacion del login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,16 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in?
-  
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-  
-    def logged_in?
-      !!current_user
-    end
-  
-    def require_user
-      unless logged_in?
-        flash[:alert] = 'Debes iniciar sesión para acceder a esta página.'
-        redirect_to root_path
-      end
-    end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre_usuario, :ubication_id, :state_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nombre_usuario, :ubication_id, :state_id])
   end
+
+end
+
+
+
+
